32370dfe0620aebf41f73f7b80b2ef7e4a4055d2
charlieboygis <charlieboygis@gmail.com>
2022-02-05 23:58:27 +0100

    fix bound structure

--- src/bounds.js
+++ src/bounds.js
@@ -1,8 +1,9 @@
 export default class Bounds {
   constructor(object) {
-    this.minlat = object.minlat;
-    this.minlon = object.minlon;
-    this.maxlat = object.maxlat;
-    this.maxlon = object.maxlon;
+    this.$ = {};
+    this.$.minlat = object.minlat;
+    this.$.minlon = object.minlon;
+    this.$.maxlat = object.maxlat;
+    this.$.maxlon = object.maxlon;
   }
 }
